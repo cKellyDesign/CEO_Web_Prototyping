a5c8e43f7ce60b9693fcfbaf3df4d8469f61b579
ckellydesign <ckellydesign.net@gmail.com>
2017-07-23 16:43:25 -0700

    Installing Django@1.8.6
    `$ pip install Django==1.8.6`

---
+++ Django_Firebase_Proto/Django-Firebase/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/Conor/CEO_Web_Prototyping/Django_Firebase_Proto/Django-Firebase/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
